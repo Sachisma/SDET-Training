082ddcdc2c075c15748094785eac98590dcb7b99
Sachisma <73516445+Sachisma@users.noreply.github.com>
2020-11-03 11:28:19 +0530

    Add files via upload

---
+++ Practice5.py
@@ -0,0 +1,22 @@
+a=5
+b=6
+c=7
+
+if a > b:
+  if a > c:
+    print("a is the greatest")
+  else:
+    print("c is the greatest")
+elif b > a:
+  if b > c:
+    print("b is the greatest")
+  else:
+    print("c is the greatest")
+
+  	
+if a > b and a > c:
+     print("a is greatest")
+elif b > a and b > c:
+     print ("b is greatest")
+elif c > a and c > b:
+     print("c is greatest")   
