4428db7ce2f2dee31189b76b83f1f82b578ebea6
Sachisma <73516445+Sachisma@users.noreply.github.com>
2020-11-05 13:37:17 +0530

    Delete Activity10.py

--- Activity10.py
+++
@@ -1,7 +0,0 @@
-	
-num_tuple = (22, 24, 33, 40, 85)	
-print("Given list is ", num_tuple)
-print("Elements that are divisible by 5:")
-for num in num_tuple:
-    if (num % 5 == 0):
-        print(num)
