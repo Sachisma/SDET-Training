361a53141bf82ed2f37e31abdfc25d109de62ea2
Sachisma <73516445+Sachisma@users.noreply.github.com>
2020-12-08 16:26:42 +0530

    Update Activity1_2.java

--- Appium/Appium Project/Activity1_2.java
+++ Appium/Appium Project/Activity1_2.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+
+import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
@@ -21,7 +23,7 @@ public class Activity1_2 {
 		  WebDriverWait wait;
 		    AppiumDriver<MobileElement> driver = null;
 
-		    @BeforeTest
+		    @BeforeClass
 		    public void setup() throws MalformedURLException {
 
 		        // Set the Desired Capabilities
@@ -70,8 +72,8 @@ public class Activity1_2 {
 		         
 		     
 		     }
-		 @AfterTest
+		 @AfterClass
 		    public void tearDown() {
-		        driver.quit();
+		       // driver.quit();
 		    }
 }
